a9c19b7640587b93a5dad7d83c07f205da95d504
Icariami <gosiamak11@gmail.com>
2024-03-21 20:16:25 +0100

    spaces added

--- Lab_02/zad2.py
+++ Lab_02/zad2.py
@@ -2,21 +2,17 @@ from random import choice
 # import copy
 from zad1 import draw, G
 
+
 def randomize_graph(G):
-    first_edge=choice(list(G.edges()))
+    first_edge = choice(list(G.edges()))
     print(*first_edge)
-    second_edge=choice(list(G.edges()))
+    second_edge = choice(list(G.edges()))
     while second_edge[0] in first_edge or second_edge[1] in first_edge:
-        second_edge=choice(list(G.edges()))
+        second_edge = choice(list(G.edges()))
     G.remove_edge(*second_edge)
-    G.add_edge(first_edge[0],second_edge[0])
-    G.add_edge(first_edge[1],second_edge[1])
+    G.add_edge(first_edge[0], second_edge[0])
+    G.add_edge(first_edge[1], second_edge[1])
     return G
 
 
 draw(randomize_graph(G))
-
-    
-
-
-
--- Lab_02/zad3.py
+++ Lab_02/zad3.py
@@ -1,31 +1,30 @@
 from zad2 import G
 
-def bfs(G,node):
-    visited=[]
-    queue=[]
+
+def bfs(G, node):
+    visited = []
+    queue = []
     visited.append(node)
     queue.append(node)
     while queue:
-        m=queue.pop(0)
+        m = queue.pop(0)
         for neighbor in G[m]:
             if neighbor not in visited:
                 visited.append(neighbor)
                 queue.append(neighbor)
     return visited
 
+
 def coherent_G_el(G):
-    coherent_list_of_all=[]
-    visited=set()
+    coherent_list_of_all = []
+    visited = set()
     for node in G.nodes():
         if node not in visited:
-            curr_coherent=bfs(G,node)
+            curr_coherent = bfs(G, node)
             coherent_list_of_all.append(curr_coherent)
             visited.update(curr_coherent)
     return coherent_list_of_all
 
 
-coherent_list_of_all=coherent_G_el(G)
+coherent_list_of_all = coherent_G_el(G)
 print(coherent_list_of_all)
-
-
-
