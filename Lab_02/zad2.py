2c9d714be5c068a54df13e419b0d95e88e635a4d
Icariami <gosiamak11@gmail.com>
2024-03-21 21:19:21 +0100

    prawie

--- Lab_02/zad2.py
+++ Lab_02/zad2.py
@@ -7,9 +7,13 @@ def randomize_graph(G):
     first_edge = choice(list(G.edges()))
     print(*first_edge)
     second_edge = choice(list(G.edges()))
+    print(*second_edge)
     while second_edge[0] in first_edge or second_edge[1] in first_edge:
         second_edge = choice(list(G.edges()))
     G.remove_edge(*second_edge)
+    G.remove_edge(*first_edge)
+    print(first_edge[0], second_edge[0])
+    print(first_edge[1], second_edge[1])
     G.add_edge(first_edge[0], second_edge[0])
     G.add_edge(first_edge[1], second_edge[1])
     return G
