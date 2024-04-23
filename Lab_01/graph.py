3663d6b0b05e7045d91fddce75077ca8c2e533e0
Wiktor Szewczyk <wiktorsz2002@gmail.com>
2024-04-24 00:11:02 +0200

    Move drawing to Graph class, final ex3

--- Lab_01/graph.py
+++ Lab_01/graph.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 from converter import convert_adjacency_matrix_to_adjacency_list, convert_incidence_matrix_to_adjacency_list
 
 
@@ -11,9 +12,10 @@ class Graph:
         self.representation_type = representation_type
 
     def import_graph(self):
-        with open(self.file_path, "r") as file:
-            for line in file:
-                self.graph.append(list(map(int, line.split())))
+        if self.file_path is not None:
+            with open(self.file_path, "r") as file:
+                for line in file:
+                    self.graph.append(list(map(int, line.split())))
 
     def __iter__(self):
         return iter(self.graph)
@@ -21,7 +23,7 @@ class Graph:
     def __str__(self):
         return str(self.graph)
 
-    def plot(self):
+    def plot(self, save=False):
         graph = self.graph
         if self.representation_type == "AM":
             graph = convert_adjacency_matrix_to_adjacency_list(self.graph)
@@ -45,6 +47,31 @@ class Graph:
 
         plt.axis('equal')
         plt.axis('off')
-        plt.show()
+
+        if save:
+            plt.savefig("graph.png")
+        else:
+            plt.show()
 
     # TODO: Implement checking if provided representation type is valid with provided file
+
+
+class RandomGraph(Graph):
+    def __init__(self, num_nodes=random.randint(1, 10), probability=None):
+        super().__init__('AL', None)
+        self.num_nodes = num_nodes
+        self.probability = probability
+        self.graph = self.generate_random_graph()
+
+    def generate_random_graph(self):
+        graph = [[0 for _ in range(self.num_nodes)] for _ in range(self.num_nodes)]
+        for i in range(self.num_nodes):
+            for j in range(i + 1, self.num_nodes):
+                if random.choice([True, False]) if self.probability is None else random.random() > self.probability:
+                    graph[i][j] = graph[j][i] = 1
+        return graph
+
+    def save(self):
+        with open("random_graph", "w") as file:
+            for row in self.graph:
+                file.write(" ".join(map(str, row)) + "\n")
--- Lab_01/lab01.py
+++ Lab_01/lab01.py
@@ -1,11 +1,12 @@
 from converter_tests import *
-from graph import Graph
+from graph import Graph, RandomGraph
 import sys
-from argparse import ArgumentParser
+import argparse
 
 
 def main():
-    parser = ArgumentParser()
+    parser = argparse.ArgumentParser(description="Graph representation converter", add_help=False)
+    parser.add_argument("-h", "--help", action="help", default=argparse.SUPPRESS, help="Display this manual")
     parser.add_argument("--caltam", action="store_true", help="Test convert adjacency list to adjacency matrix function")
     parser.add_argument("--camtal", action="store_true", help="Test convert adjacency matrix to adjacency list function")
     parser.add_argument("--caltim", action="store_true", help="Test convert adjacency list to incidence matrix function")
@@ -15,15 +16,11 @@ def main():
     parser.add_argument("-a", "--all", action="store_true", help="Run all tests")
     parser.add_argument("-f", "--file", nargs=2, help="Import graph from file")
     parser.add_argument("-d", "--draw", action="store_true", help="Draw graph from imported file")
-    parser.add_argument("-r", "--random", action="store_true", help="Generate random graph")
+    parser.add_argument("-r", "--random", nargs="+", help="Generate random graph [NUM_NODES] [p PROBABILITY] else random number of nodes and edges without probability")
     parser.add_argument("-s", "--save", action="store_true", help="Save generated graph to file")
-    parser.add_argument("-h", "--help", action="store_true", help="Display this help and exit")
+    parser.add_argument("-sp", "--save_plot", action="store_true", help="Save plot to file")
 
     args = parser.parse_args()
-
-    if args.help:
-        parser.print_help()
-        sys.exit(0)
 
     if args.all:
         convert_adjacency_list_to_adjacency_matrix_test()
@@ -61,18 +58,53 @@ def main():
     if args.file:
         graph = Graph(args.file[0], args.file[1])
         if args.draw:
-            graph.plot()
+            if args.save_plot:
+                graph.plot(save=True)
+            else:
+                graph.plot()
         else:
             raise ValueError("No action specified")
         sys.exit(0)
 
-    # if args.random:
-    #     graph = RandomGraph()
-    #     if args.save:
-    #         graph.save()
-    #     if args.draw:
-    #         graph.draw()
-    #     sys.exit(0)
+    if args.random:
+        if args.random is not True:
+            if len(args.random) == 1:
+                if int(args.random[0]) < 1:
+                    raise ValueError("Invalid number of nodes")
+                graph = RandomGraph(int(args.random[0]))
+            elif len(args.random) == 2:
+                if args.random[0] == "p":
+                    if 0 <= float(args.random[1]) < 1:
+                        graph = RandomGraph(probability=float(args.random[1]))
+                    else:
+                        raise ValueError("Invalid probability")
+            elif len(args.random) == 3:
+                if args.random[1] == "p":
+                    if 0 <= float(args.random[2]) < 1:
+                        if int(args.random[0]) < 1:
+                            raise ValueError("Invalid number of nodes")
+                        graph = RandomGraph(int(args.random[0]), float(args.random[2]))
+                    else:
+                        raise ValueError("Invalid probability")
+            else:
+                raise ValueError("Invalid arguments")
+        else:
+            graph = RandomGraph()
+        if args.save:
+            graph.save()
+        if args.draw:
+            if args.save_plot:
+                graph.plot(save=True)
+            else:
+                graph.plot()
+        else:
+            print(graph)
+        sys.exit(0)
+
+    if not any(vars(args).values()):
+        parser.print_help()
+        sys.exit(1)
+
 
 if __name__ == "__main__":
     main()
