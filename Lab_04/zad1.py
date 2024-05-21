8352f0cfd40ecee5e4ad5cdcb34ad16767e3392d
unknown <mfigurska@student.agh.edu.pl>
2024-05-22 00:18:50 +0200

    3 done

--- Lab_04/zad1.py
+++ Lab_04/zad1.py
@@ -2,31 +2,40 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import random
 
+
 def directed_graph(n, p):
     graph = []
     for i in range(n):
-        graph.append([])
+        graph.append([]) # pusta lista sąsiedztwa dla wierzcholka
         for j in range(n):
             if i != j:
-                rand = random.random()
-                if rand < p:
-                    graph[i].append(j)
-        graph[i] = tuple(graph[i])
-    print(graph)
+                rand = random.random()  # losowanie liczby z przedzialu [0, 1)
+                if rand < p: 
+                    graph[i].append(j)   # dodanie krawedzi jesli wylosowana liczba jest mniejsza od p
+        graph[i] = tuple(graph[i]) 
     return graph
 
-def draw(G, n):
-    graph = nx.DiGraph()
+
+def print_graph(graph):
+    for i, el in enumerate(graph):
+        print(f"{i}: {el}")
+
+
+def draw(G):
+    n = len(G)
+    graph = nx.DiGraph() 
     for i in range(n):
         for j in G[i]:
-            graph.add_edge(i,j)
-    nx.draw(graph, with_labels=True, font_weight='bold')
-    filename = "graph"+ ".png"
+            graph.add_edge(i, j)  # dodanie krawedzi z i do j
+    nx.draw(graph, with_labels=True, font_weight="bold")
+    filename = "graph" + ".png"
     plt.savefig(filename)
-    # plt.show()
     plt.close()
 
 
 if __name__ == "__main__":
-    G = directed_graph(9, 0.3)
-    draw(G, 5 )
+    n = 5         #ilosc wierzcholkow
+    p = 0.3       #prawdopodobienstwo 
+    G = directed_graph(n, p)
+    print_graph(G)      #wypisanie listy sasiedztwa
+    draw(G)
--- Lab_04/zad2.py
+++ Lab_04/zad2.py
@@ -3,15 +3,17 @@ from collections import defaultdict
 
 t = 0
 
+
 def DFS(G, v, visited, performed):
     global t
     t = t + 1
-    visited[v] = t
+    visited[v] = t 
     for i in G[v]:
-        if visited[i] == -1:
-            DFS(G, i, visited, performed)
+        if visited[i] == -1:  
+            DFS(G, i, visited, performed) 
     t = t + 1
     performed[v] = t
+
 
 def transform_graph(G):
     G_t = [[] for _ in range(len(G))]
@@ -20,15 +22,17 @@ def transform_graph(G):
             G_t[j].append(i)
     return G_t
 
+
 def components(n, i, G, comp):
     for j in G[i]:
         if comp[j] == -1:
-            comp[j] = n
+            comp[j] = n 
             components(n, j, G, comp)
 
+
 def kosoraju_algorithm(G):
-    visited = [-1] * len(G)
-    performed = [-1] * len(G)
+    visited = [-1] * len(G) # wierzchołek nie był odwiedzony 
+    performed = [-1] * len(G) # wierzchołek nie był przetworzony
 
     for i in range(len(G)):
         if visited[i] == -1:
@@ -37,10 +41,10 @@ def kosoraju_algorithm(G):
     G_t = transform_graph(G)
 
     n = 0
-    comp = [-1] * len(G)
+    comp = [-1] * len(G) 
 
     performed = [(x, i) for i, x in enumerate(performed)]
-    performed.sort(reverse = True, key = lambda x: x[0])
+    performed.sort(reverse=True, key=lambda x: x[0]) 
 
     for val, i in performed:
         if comp[i] == -1:
@@ -52,10 +56,10 @@ def kosoraju_algorithm(G):
     for i, el in enumerate(comp):
         results[el].append(i)
 
-    return results
+    print(results)
+
 
 if __name__ == "__main__":
     G = zad1.directed_graph(5, 0.3)
     zad1.draw(G, 5)
-    result_dict = kosoraju_algorithm(G)
-    print(result_dict)
+    kosoraju_algorithm(G)
